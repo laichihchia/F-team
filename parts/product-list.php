41cce3bc1484dc501e6c756112f2720315efb1ab
cooler0713 <qqoogg00@yahoo.com.tw>
2022-06-02 13:30:39 +0800

    cooler列表更新

--- parts/product-list.php
+++ parts/product-list.php
@@ -4,7 +4,8 @@
         <a href="#" class="list-a list-group-item list-group-item-action <?= $pageName === "Login" ? 'active' : '';?>">登入/註冊</a>
         <a href="#" class="list-a list-group-item list-group-item-action">會員管理</a>
         <a href="#" class="list-a list-group-item list-group-item-action">商品列表</a>
-        <a href="#" class="list-a list-group-item list-group-item-action">課程資訊</a>
+        <a href="#" class="list-a list-group-item list-group-item-action <?= $pageName === "課程資訊" ? 'active' : '';?>">課程資訊</a>
+
         <a href="#" class="list-a list-group-item list-group-item-action <?= $pageName === "Nathan's cart" ? 'active' : '';?>">購物車</a>
     </div>
 </div>
