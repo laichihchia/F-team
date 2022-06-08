48cecdbbd59cb580cb7f0f62692d0825fa6125b3
NathanLai <a403440322@gmail.com>
2022-06-08 22:57:53 +0800

    new page unready

--- parts/product-list.php
+++ parts/product-list.php
@@ -175,6 +175,7 @@ if (isset($_SESSION['user'])) {
             <a href="#" class="list-a list-group-item list-group-item-action">商品列表</a>
             <a href="cooler-list.php" class="list-a list-group-item list-group-item-action <?= $pageName === "課程資訊" ? 'active' : ''; ?>">課程資訊</a>
             <a href="Nathan-CartList.php" class="list-a list-group-item list-group-item-action <?= $pageName === "Nathan's cart" ? 'active' : ''; ?>">購物車</a>
+            <a href="Nathan-Orders.php" class="list-a list-group-item list-group-item-action <?= $pageName === 'Nathan-orders' ? 'active' : ''; ?>">訂單資訊</a>
 
         </div>
     </div>
