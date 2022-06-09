6aee3c109f06c996d2115168cad56f0cd3f7862a
Gary <garylin0969@gmail.com>
2022-06-09 23:26:40 +0800

    0609

--- gary-mem-list-truetest.php
+++ gary-mem-list-truetest.php
@@ -7,6 +7,20 @@ $perPage = 15; //每一頁有幾筆
 //頁碼 用戶要看第幾頁
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
 
+$sid = isset($_GET['sid']) ? intval($_GET['sid']) : 1;
+
+
+// 如果不是空字串或者是0
+if (!empty($sid)) {
+    $rec_sql = $pdo->query("SELECT `orders`.member_sid, `order_details`.*, `produst`.name, `produst`.img, `orders`.order_date FROM orders
+        JOIN order_details
+            ON `orders`.sid=`order_details`.order_sid
+        JOIN produst
+            ON `produst`.sid=`order_details`.produst_sid
+        WHERE `orders`.member_sid= $sid
+        ORDER BY `orders`.order_date DESC, `order_details`.sid;")->fetchAll();
+}
+
 // 如果頁碼小於1 轉頁面到第一頁
 if ($page < 1) {
     header('Location: ?page=1');
@@ -321,7 +335,7 @@ if ($totalRows > 0) {
     }
 
     function rec_it(sid) {
-        location.href = `gary-member-rec.php?sid=${sid}`;
+        location.href = `gary-mem-list-truetest.php?sid=${sid}`;
     }
 
     // 全選checkbox同步設定
